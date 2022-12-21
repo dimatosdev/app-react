d4453461275034514ee09e0a3d3dc6998845004b
Diego de Matos Silva <dimatoseng@gmail.com>
2022-12-21 13:10:00 -0300

    Lesson 8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
+  const nome = "Maria"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
+      <SayMyName nome = "Diego"/>
+      <SayMyName nome = "Gabriel"/>
+      <SayMyName nome = {nome}/>
+      <Pessoa 
+      nome="Diego" 
+      idade="28" 
+      profissao="Dev" 
+      foto="https://via.placeholder.com/150"/>
     </div>
+    
   );
 }
 
