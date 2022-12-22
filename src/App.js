ab11d44787605f3cbc668d45d742b54499b99efb
Diego de Matos Silva <dimatoseng@gmail.com>
2022-12-21 21:33:04 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './App.css';
-import OutherList from './components/OutherList';
-
+import React from 'react'
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
-
-  const myItens = ['react', 'Vue', 'Angular']
-
   return (
     <div className="App">
-      <h1>Renderização de listas</h1>
-      <OutherList itens={myItens}/>
-      <OutherList itens={[]}/>
+      <header className="App-header">
+        
+        <Router>
+        <NavBar/>
+          <Routes>
+            <Route path='/' exact='true' element={<Home/>}></Route>
+            <Route path='/empresa' element={<Empresa/>}></Route>
+            <Route path='/contato' element={<Contato/>}></Route>
+          </Routes>
+        </Router>
+
+      </header>
     </div>
-    
   );
 }
 
---
+++ src/pages/Contato.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Contato() {
+    return (
+        <div>
+            <h1>Contato</h1>
+            <p>Página Contato</p>
+        </div>
+    )
+}
+
+export default Contato
